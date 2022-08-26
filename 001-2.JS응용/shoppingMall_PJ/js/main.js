a723e069373921838a24112c6057c052ab02753b
Tom B. Parker <tombap8@hotmail.com>
2022-08-26 18:59:48 +0900

    Update main.js

--- 001-2.JS응용/shoppingMall_PJ/js/main.js
+++ 001-2.JS응용/shoppingMall_PJ/js/main.js
@@ -48,18 +48,18 @@ function loadFn() {
     let slide = document.querySelector('#slide');
 
     // 광클금지 상태변수
-    let prot = 0;//1-금지,0-허용
+    let prot = 0; //1-금지,0-허용
 
     // 3. 클릭이벤트 기능구현
 
     // 3-1.오른쪽버튼
-    abtn[1].onclick = ()=>{
+    abtn[1].onclick = () => {
 
         //////////// 광클금지 ////////////
-        if(prot) return; // 리턴으로 나감!
+        if (prot) return; // 리턴으로 나감!
         // prot===1 이라고 안해도 1이면 true!
         // 1은 true, 0은 false와 서로 교차됨!
-        prot = 1;// 첫번째 신호가 잠금!
+        prot = 1; // 첫번째 신호가 잠금!
         // 0.4초후 해제
         setTimeout(() => prot = 0, 400);
         //////////////////////////////////
@@ -70,11 +70,11 @@ function loadFn() {
         // 3. 기능구현: left값을 -100%로 변경
         // 기능1단계 - 왼쪽으로 슬라이드 하나만큼 나가기!+트랜지션
         slide.style.left = '-100%';
-        slide.style.transition = 
-        'left .4s ease-in-out';
+        slide.style.transition =
+            'left .4s ease-in-out';
         // 기능2단계 - 슬라이드 이동후 맨앞li 맨뒤이동
         // 0.4초후 실행하려면? setTimeout(함수,시간)
-        setTimeout(()=>{
+        setTimeout(() => {
             // 맨앞요소 맨뒤이동
             slide.appendChild(
                 slide.querySelectorAll('li')[0]);
@@ -83,9 +83,9 @@ function loadFn() {
             // left가 0이될때 트랜지션 지우기!
             slide.style.transition = 'none';
 
-        },400); // 400은 0.4초
+        }, 400); // 400은 0.4초
 
-        
+
         // 블릿변경함수 호출(1전달!)
         chgIndic(1);
 
@@ -93,13 +93,13 @@ function loadFn() {
     }; //////////// 오른쪽 버튼 click //////////
 
     // 3-2.왼쪽버튼
-    abtn[0].onclick = ()=>{
+    abtn[0].onclick = () => {
 
         //////////// 광클금지 ////////////
-        if(prot) return; // 리턴으로 나감!
+        if (prot) return; // 리턴으로 나감!
         // prot===1 이라고 안해도 1이면 true!
         // 1은 true, 0은 false와 서로 교차됨!
-        prot = 1;// 첫번째 신호가 잠금!
+        prot = 1; // 첫번째 신호가 잠금!
         // 0.41초후 해제
         setTimeout(() => prot = 0, 410);
         //////////////////////////////////
@@ -112,7 +112,7 @@ function loadFn() {
         // li요소
         let lis = slide.querySelectorAll('li');
         // 맨뒤요소 맨앞 이동 : insertBefore(넣을놈,넣을놈전놈)
-        slide.insertBefore(lis[lis.length-1],lis[0]);
+        slide.insertBefore(lis[lis.length - 1], lis[0]);
         // left값을 -100%로 변경하기
         slide.style.left = '-100%';
         // 트랜지션 없애기
@@ -125,10 +125,10 @@ function loadFn() {
             // left값을 0으로변경
             slide.style.left = '0';
             // 트랜지션 설정
-            slide.style.transition = 
-            'left .4s ease-in-out';
+            slide.style.transition =
+                'left .4s ease-in-out';
 
-        },10); // 0.01초후에 실행함! ///
+        }, 10); // 0.01초후에 실행함! ///
 
         // 블릿변경함수 호출(0전달!)
         chgIndic(0);
@@ -142,8 +142,8 @@ function loadFn() {
     // 넣는 방법은 처음로딩후 바로 li순번을 넣는다!
     // forEach문 사용!!!
     let sld = slide.querySelectorAll('li');
-    sld.forEach((ele,idx)=>{ // ele - 각요소, idx - 순번
-        ele.setAttribute('data-seq',idx);
+    sld.forEach((ele, idx) => { // ele - 각요소, idx - 순번
+        ele.setAttribute('data-seq', idx);
         // 속성명을 'data-'로 시작하면 내가만든 속성명을
         // 사용할 수 있도록 w3c에서 지정함!
         // setAttribute(속성명, 속성값) -> 속성셋팅메서드
@@ -155,24 +155,24 @@ function loadFn() {
     ///// 블릿의 표시를 해당 슬라이드 순번과 같은
     // 블릿에 class="on"을 주면 회색이미지로 보임!
     // 나머지는 모두 on을 빼야함!
-    function chgIndic(num){ 
+    function chgIndic(num) {
         // num - 읽을 슬라이드 순번
         // 오른쪽버튼은 1, 왼쪽버튼은 0을 전달!
 
         // 1. 호출확인!
-        console.log('블릿:',num);
+        console.log('블릿:', num);
 
         // 2. 슬라이드 속성 'data-seq'값 읽어오기
-        let seq = 
-        slide.querySelectorAll('li')[num]
-        .getAttribute('data-seq');
+        let seq =
+            slide.querySelectorAll('li')[num]
+            .getAttribute('data-seq');
 
-        
+
         // 값확인
-        console.log('data-seq:',seq);
+        console.log('data-seq:', seq);
 
         // 3. 블릿 클래스 초기화!
-        indic.forEach((ele)=>{
+        indic.forEach((ele) => {
             ele.classList.remove('on');
         }); ////// forEach ////////////
 
@@ -188,10 +188,9 @@ function loadFn() {
             3) toggle(클래스명) - 클래스추가/제거
         */
 
-
-
-
     } ///////////// chgIndic 함수 ////////
-    
+
+
+
 } //////////////// loadFn 함수 ///////////////
 /////////////////////////////////////////////
